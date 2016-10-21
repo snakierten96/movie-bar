e1c4b400d93f7abc270dd08642f178e679f2ce7f
snakierten96 <neil.lunn1@gmail.com>
2016-10-21 03:25:58 +0000

    add interim types

--- src/app/movies/movie.types.ts
+++ src/app/movies/movie.types.ts
@@ -31,5 +31,17 @@ export interface IMovie {
   mpa_rating?: string,
   backround_image?: string,
   backround_image_original?: string,
-  small_cover_image?: string
+  small_cover_image?: string,
+  large_cover_image?: string,
+  state: string,
+  torrents?: ITorrent[],
+  date_uploaded: string,
+  date_uploaded_unix: number
+}
+
+export interface IMovieResponse {
+  movie_count?: number,
+  limit?: number,
+  page_number?: number,
+  movies?: IMovie[]
 }
