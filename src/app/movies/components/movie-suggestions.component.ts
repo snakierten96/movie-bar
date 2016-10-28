3da998f6ec2b81977140b800ff77c1154eb4e113
neil.lunn <neil.lunn>
2016-10-28 12:13:48 +1100

    refactor router links for templates; handle component changes

--- src/app/movies/components/movie-suggestions.component.ts
+++ src/app/movies/components/movie-suggestions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChange, Input } from '@angular/core';
 import { MovieSuggestionsService } from '../services';
 
 @Component({
@@ -6,14 +6,32 @@ import { MovieSuggestionsService } from '../services';
   templateUrl: './movie-suggestions.component.html',
   styleUrls: ['./movie-suggestions.component.scss']
 })
-export class MovieSuggestionsComponent implements OnInit {
+export class MovieSuggestionsComponent implements OnInit, OnChanges {
 
   @Input() movieId;
   movies: Object[];
 
   constructor( private movieSuggestionsService: MovieSuggestionsService) { }
 
-  ngOnInit() {
+  ngOnInit () {
+    this.loadSuggestions();
+  }
+
+  ngOnChanges (changes: {[propertyName: string]: SimpleChange}) {
+    for (let propName in changes) {
+      let chng = changes[propName];
+      let cur = JSON.stringify(chng.currentValue);
+      let prev = JSON.stringify(chng.previousValue);
+      console.log(+chng.previousValue > 0);
+      console.log(`${propName}: current = ${cur}, previous = ${prev}`);
+      // Ignore initial change, but fire on every other change
+      if ( propName === 'movieId' && +chng.previousValue > 0) {
+        this.loadSuggestions();
+      }
+    }
+  }
+
+  loadSuggestions () {
     this.movieSuggestionsService.getSuggestions(this.movieId)
       .subscribe(
         response => this.movies = response,
