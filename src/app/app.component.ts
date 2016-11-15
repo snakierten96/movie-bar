2ef7791724163bc39317a7c35ac55c1ad1aab24b
neil.lunn <neil.lunn>
2016-11-16 07:13:54 +1100

    initial navigation loading implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router, Event, NavigationStart, 
+         NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'mb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent {
   private title: string = "Movie Bar";
+  loading: boolean = true;
 
   navItems: Object[] = [
     { path: ['/'], description: 'Home', icon: 'home' },
@@ -16,7 +18,20 @@ export class AppComponent {
     { path: ['/', 'movie'], description: 'Movie Detail', icon: 'fingerprint' }
   ];
 
-  constructor(private titleService: Title) {
+  constructor(private router: Router, private titleService: Title) {
     this.titleService.setTitle(this.title);
+    router.events.subscribe((event: Event) => this.navigationInterceptor(event));
   }
+
+  navigationInterceptor(event: Event): void {
+
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof (NavigationEnd || NavigationCancel || NavigationError)) {
+      this.loading = false;
+    }
+
+  }
+
 }
